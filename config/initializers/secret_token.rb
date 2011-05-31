c98200f9b412345efa65c16de6bac58bba808420
Parker Short <Parker.short@gmail.com>
2011-05-31 18:28:41 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '8d271a5c2e0f614e9b896bb0c5c72911c5a0582ad2f7942aceb68ff032b77d156d1aae5c2f5a056ddfc9ef24f07bbce684ee4bb53a83c7e0c03d58c0241bbb4f'
